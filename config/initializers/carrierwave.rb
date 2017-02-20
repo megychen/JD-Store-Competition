c7bb8fa32091fac4ae8742afd420793754c5ea28
megy <megy@megys-MacBook-Pro.local>
2017-02-20 12:37:07 +0800

    competiotion v3-megy

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,3 @@
-
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
@@ -7,15 +6,22 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],      # 你的 key
+
 
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],      # 你的 secret key
 
-      region: ENV["ap-northeast-1"]
+
+      region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
+
 
     }
+
     config.fog_directory  = ENV["AWS_BUCKET_NAME"] # 你设定的 bucket name
+
+
+
   else
     config.storage :file
   end
