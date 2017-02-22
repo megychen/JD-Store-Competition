1f926dbb6ae2ad45d398c37eb4aeca688687cb41
chris1203 <liuhuiling1203@gmail.com>
2017-02-23 00:01:33 +0800

    decorate welcome page code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,27 +79,5 @@ $(".hover").mouseleave(
   }(jQuery));
 
 
-
-
-
-
-// $('section.awSlider .carousel').carousel({
-// 	pause: "hover",
-//   interval: 2000
-// });
-//
-// var startImage = $('section.awSlider .item.active > img').attr('src');
-// $('section.awSlider').append('<img src="' + startImage + '">');
-//
-// $('section.awSlider .carousel').on('slid.bs.carousel', function () {
-//  var bscn = $(this).find('.item.active > img').attr('src');
-// 	$('section.awSlider > img').attr('src',bscn);
-// });
-
-
-/*
-Philips ambilight tv
-Üzerine gleince duruyor slide
-*/
-
+/* star rating  */
 $('#star-rating img').eq(0).trigger('click')
