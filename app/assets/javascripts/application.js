a425942fa2d9a062a0f0b36b1e310677610035b1
megy <megy@megys-MacBook-Pro.local>
2017-03-03 22:12:22 +0800

    implement index go page top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,9 @@
 //= require social-share-button/wechat
 //= require_tree .
 
+
+
+
 /* welcome-page pins*/
 
 $(function(){
@@ -163,3 +166,8 @@ function removeItem(removeButton)
     recalculateCart();
   });
 }
+
+
+$(document).on('click', '.index', function () {
+  $('body').animate({'scrollTop': 0}, 700) 
+})
